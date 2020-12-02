c5e63517128bece03893bc20ef014a589ae062b2
Viktoria Gordeeva <viktoria71311@gmail.com>
2020-12-02 19:28:59 +0200

    added apdate on location

--- js/placeService.js
+++ js/placeService.js
@@ -2,7 +2,7 @@
 
 
 function initMap(lat, lng) {
-    if (!lat||!lang) lat = 29.5581, lng = 34.9482;
+    if (!lat || !lang) lat = 29.5581, lng = 34.9482;
     var elMap = document.querySelector('#map');
     var options = {
         center: { lat, lng },
@@ -25,4 +25,43 @@ function initMap(lat, lng) {
     //     const placeName = prompt('Place name?')
     //     console.log('Map clicked', placeName, ev.latLng.lat(), ev.latLng.lng());
     // })
+}
+
+function getPosition() {
+    if (!navigator.geolocation) {
+        alert("HTML5 Geolocation is not supported in your browser.");
+        return;
+    }
+    navigator.geolocation.getCurrentPosition(showLocation, handleLocationError);
+}
+
+function showLocation(position) {
+    console.log(position);
+    // document.getElementById("latitude").innerHTML = position.coords.latitude;
+    // document.getElementById("longitude").innerHTML = position.coords.longitude;
+    // document.getElementById("accuracy").innerHTML = position.coords.accuracy;
+
+    // var date = new Date(position.timestamp);
+    // document.getElementById("timestamp").innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    // initMap(position.coords.latitude, position.coords.longitude);
+    Map.setCenter(position.coords.latitude, position.coords.longitude, 16)
+}
+
+function handleLocationError(error) {
+    // var locationError = document.getElementById("locationError");
+
+    // switch (error.code) {
+    //     case 0:
+    //         locationError.innerHTML = "There was an error while retrieving your location: " + error.message;
+    //         break;
+    //     case 1:
+    //         locationError.innerHTML = "The user didn't allow this page to retrieve a location.";
+    //         break;
+    //     case 2:
+    //         locationError.innerHTML = "The browser was unable to determine your location: " + error.message;
+    //         break;
+    //     case 3:
+    //         locationError.innerHTML = "The browser timed out before retrieving the location.";
+    //         break;
+    // }
 }
